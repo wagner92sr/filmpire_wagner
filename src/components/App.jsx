39b271736640387978086b5760590621be5c7c98
Wagner Rodrigues <wagnersr92@gmail.com>
2022-07-02 16:40:33 -0300

    libs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
-    App
+    <CssBaseline />
+    <h1>Hello, World ! - Filmpire</h1>
   </div>
 );
 
