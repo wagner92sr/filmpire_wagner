ce6d28e0bdaa6f790e48f9c5a25d8317335e6138
Wagner Rodrigues <wagnersr92@gmail.com>
2022-07-03 22:06:36 -0300

    ajustes dependencias

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Switch } from 'react-router-dom';
 
-const App = () => (
-  <div>
-    <CssBaseline />
-    <h1>Hello, World ! - Filmpire</h1>
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <h1>Hello, World ! - Filmpire</h1>
+    </div>
+  );
+}
 
 export default App;
