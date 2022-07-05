c934b058f22c400b0d0da1f1f6d1785b3f82dc2c
Wagner Rodrigues <wagnersr92@gmail.com>
2022-07-05 19:04:30 -0300

    ajustes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
-import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7} from '@mui/icons-material';
 import { Link } from 'react-router-dom';
-import { ClassNames } from '@emotion/react';
 
-function NavBar() {
-  console.log('NavBar');
+import useStyles from './styles';
+
+function NavBar = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:.600px)');
 
   return (
     <>
         <AppBar position="fixed">
-            <Toolbar className={ClassNames.toolbar}>
-
+            <Toolbar className={classes.toolbar}>
+            {isMobile && (
+              <IconButton
+              color="inherit"
+              edge="sart"
+              style ={{ outline: 'none'}}
+              onClick={() => {}}
+              className={classes.menuButton}
+              >
+                <Menu/>
+              </IconButton>
+            )}
+            
             </Toolbar>
-        <AppBar/>
+        </AppBar>
     </>
   );
 }
